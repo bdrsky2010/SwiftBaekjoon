0ebff0ddd7de0d948d2493f409e563fa77ae012c
MJKim <qorhaqt@gmail.com>
2023-10-06 23:06:15 +0900

    baekjoon

---
+++ Baekjoon/해시를 사용한 집합과 맵/(실버4) 10816 숫자 카드 2.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+var dic: [Int:Int] = [:]
+let N: Int = Int(readLine()!)!
+let have: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
+have.forEach { dic[$0] = (dic[$0] ?? 0) + 1 }
+let M: Int = Int(readLine()!)!
+let check: [Substring] = readLine()!.split(separator: " ")
+check.forEach { print(dic[Int($0)!] ?? 0, terminator: " ") }
