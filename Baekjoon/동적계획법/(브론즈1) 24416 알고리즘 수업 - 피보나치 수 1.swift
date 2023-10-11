228392faea778b5677a0480fcb6c91a94740afa2
MJKim <qorhaqt@gmail.com>
2023-10-11 22:16:22 +0900

    baekjoon

--- Baekjoon/동적계획법/(브론즈1) 24416 알고리즘 수업 - 피보나치 수 1.swift
+++ Baekjoon/동적계획법/(브론즈1) 24416 알고리즘 수업 - 피보나치 수 1.swift
@@ -1,8 +1,21 @@
-//
-//  (브론즈1) 24416 알고리즘 수업 - 피보나치 수 1.swift
-//  Baekjoon
-//
-//  Created by Minjae Kim on 10/11/23.
-//
-
 import Foundation
+
+func fibonacci1(_ n: Int, cnt: inout Int) -> Int {
+	if n == 1 || n == 2 { cnt += 1; return 1 }
+	cnt += 1
+	return fibonacci1(n - 1, cnt: &cnt) + fibonacci1(n - 2, cnt: &cnt)
+}
+
+func fibonacci2(_ n: Int) -> Int {
+	var f: [Int] = [0, 1, 1]
+	var cnt: Int = 0
+	for i in 3...n {
+		f.append(f[i - 1] + f[i - 2])
+		cnt += 1
+	}
+	return cnt
+}
+
+let input: Int = Int(readLine()!)!
+var cnt: Int = 0
+print(fibonacci1(input, cnt: &cnt), fibonacci2(input))
--- Baekjoon/동적계획법/(실버3) 9461 파도반 수열.swift
+++ Baekjoon/동적계획법/(실버3) 9461 파도반 수열.swift
@@ -1,8 +1,10 @@
-//
-//  (실버3) 9461 파도반 수열.swift
-//  Baekjoon
-//
-//  Created by Minjae Kim on 10/11/23.
-//
-
 import Foundation
+
+func P(_ n: Int) -> Int {
+	var triangle: [Int] = [0, 1, 1, 1]
+	if n <= 3 { return 1 }
+	for i in 4...n { triangle.append(triangle[i - 3] + triangle[i - 2]) }
+	return triangle[n]
+}
+let T: Int = Int(readLine()!)!
+(0..<T).forEach { _ in print(P(Int(readLine()!)!)) }
